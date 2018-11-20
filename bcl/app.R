05551a5eedecef7503f2466ccd947b2f1013d989
g-ziyan <gggongziyan@gmail.com>
2018-11-20 14:10:28 -0800

    the first attempt

--- bcl/app.R
+++ bcl/app.R
@@ -1,11 +1,15 @@
 library(shiny)
 library(ggplot2)
 library(dplyr)
+library(DT)
+library(colourpicker)
+library(shinythemes)
 
 bcl <- read.csv("bcl-data.csv", stringsAsFactors = FALSE)
 
-ui <- fluidPage(
-  titlePanel("BC Liquor Store prices"),
+ui <- fluidPage(theme = shinytheme("united"),
+  img(src = "bcl_logo.jpg", width = "500", height="180"), # reset the size of picture
+  titlePanel("BC Liquor Store Prices"),
   sidebarLayout(
     sidebarPanel(
       sliderInput("priceInput", "Price", 0, 100, c(25, 40), pre = "$"),
@@ -15,9 +19,17 @@ ui <- fluidPage(
       uiOutput("countryOutput")
     ),
     mainPanel(
-      plotOutput("coolplot"),
-      br(), br(),
-      tableOutput("results")
+      tabsetPanel(
+        tabPanel("Plot", 
+                 colourInput("col","Choose the color of the bars","red"),
+                 plotOutput("coolplot")),
+        tabPanel("Table",
+                 h5(textOutput("text")),
+                 checkboxInput("sortInput","Sort by the price",value = FALSE),
+                 DT::dataTableOutput("results"))
+      )
+      
+      
     )
   )
 )
@@ -34,24 +46,49 @@ server <- function(input, output) {
       return(NULL)
     }    
     
-    bcl %>%
+    bcl <- bcl %>%
       filter(Price >= input$priceInput[1],
              Price <= input$priceInput[2],
              Type == input$typeInput,
              Country == input$countryInput
       )
+  
+  })
+  
+  selected <- reactive({
+    if (is.null(input$countryInput)) {
+      return(NULL)
+    }    
+    
+    bcl <- bcl %>%
+      filter(Price >= input$priceInput[1],
+             Price <= input$priceInput[2],
+             Type == input$typeInput,
+             Country == input$countryInput
+      )
+    
+    if(input$sortInput) {
+      bcl %>% 
+        arrange(Price)
+    }
+    else bcl
   })
   
   output$coolplot <- renderPlot({
     if (is.null(filtered())) {
       return()
     }
-    ggplot(filtered(), aes(Alcohol_Content)) +
-      geom_histogram()
+    filtered() %>% 
+    ggplot(aes(Alcohol_Content)) +
+      geom_histogram(fill = input$col)
   })
 
-  output$results <- renderTable({
-    filtered()
+  output$results <- DT::renderDataTable({
+    selected()
+  })
+  
+  output$text <- renderText({
+    "We found options for you"
   })
 }
 
